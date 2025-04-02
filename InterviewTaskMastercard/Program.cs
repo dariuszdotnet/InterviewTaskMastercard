94647155ac08704806b6347db85df291fd5ced69
your name <you@example.com>
2025-04-02 21:57:13 +0200

    add service responsible for logic and providing the answers to the questions

--- InterviewTaskMastercard/Program.cs
+++ InterviewTaskMastercard/Program.cs
@@ -108,3 +108,71 @@ public class KingsDataProvider
         }
     }
 }
+
+/// <summary>
+/// Service responsible for providing answers to the questions related to the king's statistics.
+/// </summary>
+public class KingsLogic
+{
+    private IEnumerable<King> kings;
+
+    /// <summary>
+    /// Constructor for the <see cref="KingsLogic"/> class.
+    /// </summary>
+    /// <param name="kings"><see cref="IEnumerable{King}"/> collection containing elements representing kings.</param>
+    public KingsLogic(IEnumerable<King> kings)
+    {
+        this.kings = kings;
+    }
+
+    /// <summary>
+    /// Provides the answer to the question related to the number of monarchs in the list.
+    /// Prints question, calculates and print the answer.
+    /// </summary>
+    public void KingsCount()
+    {
+        Console.WriteLine("Question 1: How many monarchs are there in the list?");
+        Console.WriteLine($"Answer 1: There are {kings.Distinct().Count()} kings in the list.");
+    }
+
+    /// <summary>
+    /// Provides the answer to the question related to the longest ruling monarch.
+    /// Prints question, calculates and print the answer.
+    /// </summary>
+    public void LongestRulingMonarch()
+    {
+        Console.WriteLine("Question 2: Which monarch ruled the longest (and for how long)?");
+        var longestRulingKing = kings.OrderByDescending(k => k.Duration).First();
+        Console.WriteLine($"Answer 2: The monarch that rules the longest period was {longestRulingKing.Name}, who rules {longestRulingKing.Duration} years.");
+    }
+
+    /// <summary>
+    /// Provides the answer to the question related to the longest ruling house.
+    /// Prints question, calculates and print the answer.
+    /// </summary>
+    public void LongestRulingHouse()
+    {
+        Console.WriteLine("Question 3: Which house ruled the longest (and for how long)?");
+        var longestRulingHouse = kings
+            .GroupBy(k => k.House)
+            .Select(g => new { House = g.Key, Duration = g.Sum(k => k.Duration) - g.Count() }) // substract g.Count() in order to not overlap the same year in continous ruling of different kings from the same house
+            .OrderByDescending(g => g.Duration)
+            .First();
+        Console.WriteLine($"Answer 3: House that rules the longest was {longestRulingHouse.House}, who ruled {longestRulingHouse.Duration} years.");
+    }
+
+    /// <summary>
+    /// Provides the answer to the question related to the most common first name.
+    /// Prints question, calculates and print the answer.
+    /// </summary>
+    public void MostUsedFirstName()
+    {
+        Console.WriteLine("Question 4: What was the most common first name?");
+        var mostCommonFirstName = kings
+            .GroupBy(k => k.FirstName)
+            .OrderByDescending(g => g.Count())
+            .First()
+            .Key;
+        Console.WriteLine($"Answer 4: The most common first name on the list was {mostCommonFirstName}.");
+    }
+}
